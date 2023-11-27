df3ed8ac3cfb6d8c6a9b308c398d01ad5f1584b8
bryanjeshua <bryanjmt04@gmail.com>
2023-11-27 16:18:58 +0700

    init: pembagian folder modul

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:lembarpena/main.dart';
+import 'package:lembarpena/landing_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
