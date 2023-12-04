39665096211ebb556ac4fb532e46eedfcf0ef02f
pershyfon <sabrina.aviana@ui.ac.id>
2023-12-04 19:53:18 +0700

    berhasil fetch data book

--- lib/buybooks/models/cart_item.dart
+++ lib/buybooks/models/cart_item.dart
@@ -3,60 +3,62 @@
 //     final cartItem = cartItemFromJson(jsonString);
 
 import 'dart:convert';
-// import model Books
+import 'package:lembarpena/buybooks/models/book.dart';
 
-List<CartItem> cartItemFromJson(String str) => List<CartItem>.from(json.decode(str).map((x) => CartItem.fromJson(x)));
+List<CartItem> cartItemFromJson(String str) =>
+    List<CartItem>.from(json.decode(str).map((x) => CartItem.fromJson(x)));
 
-String cartItemToJson(List<CartItem> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String cartItemToJson(List<CartItem> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class CartItem {
-    String model;
-    int pk;
-    Fields fields;
+  String model;
+  int pk;
+  Fields fields;
 
-    CartItem({
-        required this.model,
-        required this.pk,
-        required this.fields,
-    });
+  CartItem({
+    required this.model,
+    required this.pk,
+    required this.fields,
+  });
 
-    factory CartItem.fromJson(Map<String, dynamic> json) => CartItem(
+  factory CartItem.fromJson(Map<String, dynamic> json) => CartItem(
         model: json["model"],
         pk: json["pk"],
         fields: Fields.fromJson(json["fields"]),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "model": model,
         "pk": pk,
         "fields": fields.toJson(),
-    };
+      };
 }
 
 class Fields {
-    int user;
-    int book; // ini nanti ganti Books
-    int quantity;
-    bool isOrdered;
+  int user;
+  Book book;
+  int quantity;
+  bool isOrdered;
 
-    Fields({
-        required this.user,
-        required this.book,
-        required this.quantity,
-        required this.isOrdered,
-    });
+  Fields({
+    required this.user,
+    required this.book,
+    required this.quantity,
+    required this.isOrdered,
+  });
 
-    factory Fields.fromJson(Map<String, dynamic> json) => Fields(
+  factory Fields.fromJson(Map<String, dynamic> json) => Fields(
         user: json["user"],
         book: json["book"],
         quantity: json["quantity"],
         isOrdered: json["is_ordered"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "user": user,
         "book": book,
         "quantity": quantity,
         "is_ordered": isOrdered,
-    };
+      };
 }
