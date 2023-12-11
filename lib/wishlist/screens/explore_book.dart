9d2f30e1ca0a497707859b21e08dc0a5b4b1a4bf
bryanjeshua <bryanjmt04@gmail.com>
2023-12-11 21:36:38 +0700

    edit: fix cia

--- lib/wishlist/screens/explore_book.dart
+++ lib/wishlist/screens/explore_book.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:lembarpena/AdminRegisterBook/widgets/admin_left_drawer.dart';
 import 'dart:convert';
-import 'package:lembarpena/AdminRegisterBook/models/book.dart';
-import 'package:lembarpena/Main/widgets/left_drawer.dart';
-import 'package:lembarpena/Wishlist/screens/detail_buku.dart';
+import 'package:lembarpena/wishlist/models/book.dart';
+import 'package:lembarpena/wishlist/screens/detail_buku.dart';
 // import 'package:pbp_django_auth/pbp_django_auth.dart';
 // import 'package:provider/provider.dart';
 
@@ -60,12 +60,11 @@ class _ExploreBooksPageState extends State<ExploreBooksPage> {
                   ],
                 );
               } else {
-                return ListView.builder(
+               return ListView.builder(
                   itemCount: snapshot.data!.length,
                   itemBuilder: (_, index) => InkWell(
                     child: Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 12),
+                      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
                       padding: const EdgeInsets.all(20.0),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.start,
@@ -127,6 +126,7 @@ class _ExploreBooksPageState extends State<ExploreBooksPage> {
                     ),
                   ),
                 );
+
               }
             }));
   }
