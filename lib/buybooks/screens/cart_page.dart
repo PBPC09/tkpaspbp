f99a4e2a3a6ca75483e89e0a9e575f2c1c98fd0f
lovaire <rifsrahmt@gmail.com>
2023-12-18 16:30:41 +0700

    coba cart

--- lib/buybooks/screens/cart_page.dart
+++ lib/buybooks/screens/cart_page.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:lembarpena/Main/widgets/left_drawer.dart';
 import 'dart:convert';
 import 'package:lembarpena/buybooks/models/cart_item.dart';
 import 'package:lembarpena/authentication/login_page.dart';
-// import 'package:lembarpena/buybooks/widgets/cart_list.dart';
+import 'package:lembarpena/Main/widgets/left_drawer.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _CartPageState createState() => _CartPageState();
 }
 
 class _CartPageState extends State<CartPage> {
+  late Future<List<CartItem>> _cartItemsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _cartItemsFuture = fetchProduct();
+  }
+
   Future<List<CartItem>> fetchProduct() async {
     String uname = LoginPage.uname;
     var url = Uri.parse('http://127.0.0.1:8000/show_cart_json/$uname/');
@@ -23,17 +29,10 @@ class _CartPageState extends State<CartPage> {
       headers: {"Content-Type": "application/json"},
     );
 
-    // melakukan decode response menjadi bentuk json
     var data = jsonDecode(utf8.decode(response.bodyBytes));
 
-    // melakukan konversi data json menjadi object Product
-    List<CartItem> listItem = [];
-    for (var d in data) {
-      if (d != null) {
-        listItem.add(CartItem.fromJson(d));
-      }
-    }
-    return listItem;
+    List<CartItem> cartItems = cartItemFromJson(jsonEncode(data));
+    return cartItems;
   }
 
   @override
@@ -43,61 +42,32 @@ class _CartPageState extends State<CartPage> {
         title: const Text('Keranjang Saya'),
       ),
       drawer: const LeftDrawer(),
-      // body: FutureBuilder(
-      //     future: fetchProduct(),
-      //     builder: (context, AsyncSnapshot snapshot) {
-      //       if (snapshot.data == null) {
-      //         return const Center(child: CircularProgressIndicator());
-      //       } else {
-      //         if (!snapshot.hasData) {
-      //           return const Column(
-      //             children: [
-      //               Text(
-      //                 "Tidak ada data produk.",
-      //                 style: TextStyle(color: Colors.redAccent, fontSize: 20),
-      //               ),
-      //               SizedBox(height: 8),
-      //             ],
-      //           );
-      //         } else {
-      //           return ListView.builder(
-      //               itemCount: snapshot.data!.length,
-      //               itemBuilder: (_, index) => InkWell(
-      //                   onTap: () {
-      //                     Navigator.push(
-      //                         context,
-      //                         MaterialPageRoute(
-      //                             builder: (context) => ItemDetailPage(
-      //                                   item: snapshot.data![index],
-      //                                 )));
-      //                   },
-      //                   child: Container(
-      //                     margin: const EdgeInsets.symmetric(
-      //                         horizontal: 16, vertical: 12),
-      //                     padding: const EdgeInsets.all(20.0),
-      //                     child: Column(
-      //                       mainAxisAlignment: MainAxisAlignment.start,
-      //                       crossAxisAlignment: CrossAxisAlignment.start,
-      //                       children: [
-      //                         Text(
-      //                           "${snapshot.data![index].fields.name}",
-      //                           style: const TextStyle(
-      //                             fontSize: 18.0,
-      //                             fontWeight: FontWeight.bold,
-      //                           ),
-      //                         ),
-      //                         const SizedBox(height: 10),
-      //                         Text("${snapshot.data![index].fields.amount}"),
-      //                         const SizedBox(height: 10),
-      //                         Text(
-      //                             "${snapshot.data![index].fields.description}")
-      //                       ],
-      //                     ),
-      //                   )));
-      //         }
-      //       }
-      //     }
-      //     )
+      body: FutureBuilder<List<CartItem>>(
+        future: _cartItemsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error.toString()}'),
+            );
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('Tidak ada barang di keranjang.'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                CartItem cartItem = snapshot.data![index];
+                return ListTile(
+                  title: Text(cartItem.fields.book.toString()),
+                  subtitle: Text('Jumlah: ${cartItem.fields.quantity.toString()}'),
+                  // Add other widgets as needed
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
