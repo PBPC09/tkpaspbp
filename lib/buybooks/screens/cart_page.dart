7103369d69125e8b7ca235e1fca209777ace4b43
bryanjeshua <bryanjmt04@gmail.com>
2023-12-19 03:13:11 +0700

    edit: kelarin kerjaan rifqi

--- lib/buybooks/screens/cart_page.dart
+++ lib/buybooks/screens/cart_page.dart
@@ -7,6 +7,8 @@ import 'package:lembarpena/buybooks/models/cart_item.dart';
 import 'package:lembarpena/authentication/login_page.dart';
 import 'package:lembarpena/wishlist/screens/explore_book.dart';
 import 'package:lembarpena/checkoutbook/screens/checkoutpage.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -19,6 +21,9 @@ class _CartPageState extends State<CartPage> {
   late Future<List<CartItem>> futureCartItems;
   final Map<int, bool> itemsChecked = {};
 
+  Future<List<CartItem>> getCartItems() {
+      return fetchCartItems();
+  }
   @override
   void initState() {
     super.initState();
@@ -28,39 +33,64 @@ class _CartPageState extends State<CartPage> {
   Future<List<CartItem>> fetchCartItems() async {
     String uname = LoginPage.uname;
     var url =
-        Uri.parse('http://127.0.0.1:8000/buybooks/show_cart_json/$uname/');
+        Uri.parse('http://localhost:8000/buybooks/show_cart_json/$uname/');
     var response =
         await http.get(url, headers: {"Content-Type": "application/json"});
+        
     if (response.statusCode == 200) {
-      var cartJson = json.decode(response.body) as List;
-      List<CartItem> cartItems =
-          cartJson.map((json) => CartItem.fromJson(json)).toList();
-      for (var item in cartItems) {
-        itemsChecked.putIfAbsent(item.id, () => false);
-      }
-      return cartItems;
+        var cartJson = json.decode(response.body) as List;
+        List<CartItem> cartItems =
+            cartJson.map((json) => CartItem.fromJson(json)).toList();
+        for (var item in cartItems) {
+          itemsChecked.putIfAbsent(item.id, () => false);
+        }
+        return cartItems;
     } else {
       throw Exception('Failed to load cart');
     }
   }
 
-  void removeItemFromCart(int itemId) {
+  Future<void> removeItemFromCart(CookieRequest request,int itemId) async {
     // Implementasi fungsi untuk menghapus item dari keranjang
-    setState(() {
-      futureCartItems = fetchCartItems();
-    });
+      final response = await request.postJson(
+          'http://localhost:8000/buybooks/delete_cart_flutter/$itemId/',
+          jsonEncode({}));
+
+      if (response['status'] == 'success') {
+        // Handle berhasil menghapus
+        setState(() {
+          // Memuat ulang data ForumHead
+          // futureCartItems = fetchCartItems();
+          ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text("Sukses dihapus!")));
+        });
+        // Muat ulang komentar
+      } else {
+        // Handle error
+      }
+
   }
 
-  void toggleCheckbox(int itemId, bool? value) {
-    if (value != null) {
-      setState(() {
-        itemsChecked[itemId] = value;
-      });
-    }
+
+  void toggleCheckbox(CookieRequest request, int itemId) async {
+        final response = await request.postJson(
+          'http://localhost:8000/buybooks/selected_flutter/$itemId/',
+          jsonEncode({}));
+
+        if (response['status'] == 'success') {
+          // Handle berhasil menghapus
+          setState(() {
+
+          });
+          // Muat ulang komentar
+        } else {
+          // Handle error
+        }
   }
 
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Cart'),
@@ -68,7 +98,7 @@ class _CartPageState extends State<CartPage> {
         foregroundColor: Colors.white,
       ),
       body: FutureBuilder<List<CartItem>>(
-        future: futureCartItems,
+        future: getCartItems(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
@@ -87,9 +117,10 @@ class _CartPageState extends State<CartPage> {
                       children: [
                         ListTile(
                           leading: Checkbox(
-                            value: itemsChecked[cartItem.id] ?? false,
+                            // value: true,
+                            value:cartItem.isSelected,
                             onChanged: (bool? value) {
-                              toggleCheckbox(cartItem.id, value);
+                              toggleCheckbox(request,cartItem.id);
                             },
                           ),
                           title: Text(
@@ -119,7 +150,7 @@ class _CartPageState extends State<CartPage> {
                                   Colors.grey.withOpacity(0.38),
                             ),
                             onPressed: () {
-                              removeItemFromCart(cartItem.id);
+                              removeItemFromCart(request,cartItem.id);
                             },
                             child: const Text('Remove'),
                           ),
--- lib/checkoutbook/screens/checkoutpage.dart
+++ lib/checkoutbook/screens/checkoutpage.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:lembarpena/Main/screens/menu.dart';
 import 'dart:convert';
 import 'package:lembarpena/authentication/login_page.dart';
 import 'package:lembarpena/buybooks/models/cart_item.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
 
 class CheckoutPage extends StatefulWidget {
   const CheckoutPage({Key? key}) : super(key: key);
@@ -13,16 +16,46 @@ class CheckoutPage extends StatefulWidget {
 
 class _CheckoutPageState extends State<CheckoutPage> {
   late Future<List<CartItem>> _cartItemsFuture;
-
+  String _groupValue = '';
+  late String uname = LoginPage.uname;
+  late Future<double> harga;
+  double valueHarga = 0;
+  TextEditingController alamatController = TextEditingController();
   @override
   void initState() {
     super.initState();
     _cartItemsFuture = fetchProduct();
+    harga = fetchHarga();
+  }
+
+  Future<List<CartItem>> getCartItems() {
+    return fetchProduct();
+  }
+
+  Future<double> fetchHarga() async {
+    var url =
+        Uri.parse('http://localhost:8000/checkoutbook/get_total_harga/$uname/');
+    var response = await http.get(
+      url,
+      headers: {"Content-Type": "application/json"},
+    );
+
+    // if (response.statusCode == 200) {
+    var data = jsonDecode(utf8.decode(response.bodyBytes));
+    var hargaStr =
+        data[0]['total_harga']; // Mengakses nilai dalam array bertingkat
+    var harga = double.tryParse(hargaStr) ??
+        0.0; // Mengonversi ke double, default ke 0.0 jika gagal
+    return harga;
+    // } else {
+    //   // Handle error atau kembalikan nilai default
+    //   return 0.0;
+    // }
   }
 
   Future<List<CartItem>> fetchProduct() async {
-    String uname = LoginPage.uname;
-    var url = Uri.parse('http://127.0.0.1:8000/show_cart_json/$uname/');
+    var url = Uri.parse(
+        'http://localhost:8000/checkoutbook/show_checkout_json/$uname/');
     var response = await http.get(
       url,
       headers: {"Content-Type": "application/json"},
@@ -39,10 +72,10 @@ class _CheckoutPageState extends State<CheckoutPage> {
     return listItem;
   }
 
-  String _groupValue = '';
-
   @override
   Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Checkout'),
@@ -50,18 +83,18 @@ class _CheckoutPageState extends State<CheckoutPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
+            const Padding(
+              padding: EdgeInsets.all(20.0),
               child: Text(
                 'Checkout',
                 style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
               ),
             ),
             FutureBuilder<List<CartItem>>(
-              future: _cartItemsFuture,
+              future: getCartItems(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
+                  return const Center(child: CircularProgressIndicator());
                 } else if (snapshot.hasError) {
                   return Center(
                     child: Text('Error: ${snapshot.error.toString()}'),
@@ -82,9 +115,27 @@ class _CheckoutPageState extends State<CheckoutPage> {
                 }
               },
             ),
+            FutureBuilder<double>(
+              future: harga, // future yang menunggu hasil
+              builder: (BuildContext context, AsyncSnapshot<double> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Text(
+                      'Loading...'); // Tampilkan loading atau widget lain saat menunggu
+                } else if (snapshot.hasError) {
+                  return Text(
+                      'Error: ${snapshot.error}'); // Tampilkan error jika ada
+                } else {
+                  // Tampilkan harga jika data tersedia
+                  valueHarga = snapshot.data!;
+                  return Text('Total Harga: ${snapshot.data}');
+                }
+              },
+            ),
+
             Padding(
               padding: const EdgeInsets.all(20.0),
               child: TextFormField(
+                controller: alamatController,
                 decoration: const InputDecoration(
                   labelText: 'Alamat',
                   border: OutlineInputBorder(),
@@ -150,9 +201,42 @@ class _CheckoutPageState extends State<CheckoutPage> {
             Padding(
               padding: const EdgeInsets.all(20.0),
               child: ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   // Implement checkout functionality
                   //nanti akan dibuat halaman selesai co
+                  final response = await request.postJson(
+                    // "http://10.0.2.2:8000/bookforum/create_question_flutter/",
+                    "http://localhost:8000/checkoutbook/checkout_flutter/",
+                    jsonEncode({
+                      "alamat": alamatController.text,
+                      "metode_pembayaran": _groupValue,
+                      "total_harga": valueHarga,
+                    }),
+                  );
+                    // Periksa kode status HTTP dari respons
+                    if (response['status'] == 'success') {
+                      // ignore: use_build_context_synchronously
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("CheckOut Berhasil!"),
+                        ),
+                      );
+                      // Navigator.pop(context); // Kembali ke halaman sebelumnya
+                      // ignore: use_build_context_synchronously
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => MyHomePage()),
+                      ); // Kembali ke halaman sebelumnya
+                    } else {
+                      // ignore: use_build_context_synchronously
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content:
+                              Text("Terdapat kesalahan, silakan coba lagi."),
+                        ),
+                      );
+                    }
                 },
                 child: const Text('Checkout'),
               ),
